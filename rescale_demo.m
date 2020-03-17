d9f6985c05d635aee4c6d317760e8dd86d126f51
Noel Brizuela <nogutier@ucsd.edu>
2020-03-17 16:11:00 -0700

    Delete rescale_demo.m

--- rescale_demo.m
+++
@@ -1,8 +0,0 @@
-function [new_var] = rescale_demo(variable, exponent)
-
-%Function to rescale demographic variables using a given exponent
-
-new_var = (variable/(mean(variable))).^exponent; 
-new_var = new_var / mean(new_var) * mean(variable); 
-
-end
